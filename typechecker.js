b5cbcb734b871972a932539ad74603918932bed3
Aylor Brown <aylorbrown@gmail.com>
2020-01-19 18:43:42 -0500

    adds checkboxes

--- typechecker.js
+++ typechecker.js
@@ -1,6 +1,13 @@
 const sentenceTag = document.querySelector(`input[type="text"]`)
+
 const typesizeTag = document.querySelector(`input[name="typesize"]`)
 const typesizeOutput = document.querySelector("span.typesize-output")
+
+const lineheightTag = document.querySelector(`input[name="lineheight"]`)
+const lineheightOutput = document.querySelector("span.lineheight-output")
+
+const italicTag = document.querySelector(`input[name="italic"]`)
+
 const outputTag = document.querySelector("textarea.output")
 const originalText = outputTag.value
 
@@ -25,4 +32,19 @@ outputTag.addEventListener("keyup", function () {
 typesizeTag.addEventListener("input", function () {
     outputTag.style.fontSize = this.value + "px"
     typesizeOutput.innerHTML = this.value + " pixels"
-  })
+})
+
+lineheightTag.addEventListener("input", function() {
+    outputTag.style.lineHeight = this.value
+    lineheightOutput.innerHTML = this.value
+})
+
+//when user clicks the italic checkbox, update the font style to 
+//either normal or italic depending if its checked or not 
+italicTag.addEventListener("change", function () {
+    if(this.checked) {
+        outputTag.style.fontStyle = "italic"
+    } else {
+        outputTag.style.fontStyle = "normal"
+    }
+})
