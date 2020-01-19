103535c7940039d2d59572a362ef01acaf95f38f
Aylor Brown <aylorbrown@gmail.com>
2020-01-19 17:38:19 -0500

    adds fallback text

--- typechecker.js
+++ typechecker.js
@@ -1,7 +1,14 @@
 const sentenceTag = document.querySelector(`input[type="text"]`)
 const outputTag = document.querySelector("section.output")
+const originalText = outputTag.innerHTML
 
 // when user types in sentenceTag, update the outputTag accordingly
+// if there;s no value, put in originalText
 sentenceTag.addEventListener("keyup", function () {
-    outputTag.innerHTML = sentenceTag.value
+    if (this.value) {
+        outputTag.innerHTML = this.value
+    // fallback value 
+    } else {
+        outputTag.innerHTML = originalText
+    }
 })
